27e7dcd67c60d5799e6c97971871dd681767c728
maygul <88988446+maygul@users.noreply.github.com>
2023-02-26 16:43:16 +0300

    Initialize
    Base Project with ready pom.xml and test sql scripts

---
+++ src/main/java/org/ptt/spring/Main.java
@@ -0,0 +1,12 @@
+package org.ptt.spring;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/resources/insert_student.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.student(id,name,surname)
+VALUES
+(nextval('public."hibernate_sequence"'),'John','Doe'),
+(nextval('public."hibernate_sequence"'),'Jayce','One');
+
+
